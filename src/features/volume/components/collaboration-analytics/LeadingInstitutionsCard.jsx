a8fde7f922ebe4566a45e8e7fc229b576cc5dbfd
KalvinKhanh <tranminhkhanh793@gmail.com>
2026-07-27 13:53:35 +0700

    fix(fe/i18n): fix UI hardcode elements, gender translations, and missing BE packages

--- src/features/volume/components/collaboration-analytics/LeadingInstitutionsCard.jsx
+++ src/features/volume/components/collaboration-analytics/LeadingInstitutionsCard.jsx
@@ -85,7 +85,6 @@ const LeadingInstitutionsCard = ({ data }) => {
               <FiBarChart2 />
             </button>
           </div>
-          <span className="ca-card-action">{t('volume.viewAll', 'VIEW ALL')}</span>
         </div>
       </div>
 
