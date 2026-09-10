4a71ab33afd78d9785bcd11fd3bafac916718095
KalvinKhanh <tranminhkhanh793@gmail.com>
2026-09-10 16:55:39 +0700

    fix filter trending page

--- src/shared/components/layout/AnalysisPreloader/AnalysisPreloader.jsx
+++ src/shared/components/layout/AnalysisPreloader/AnalysisPreloader.jsx
@@ -22,22 +22,33 @@ import './AnalysisPreloader.css';
 
 const defaultFilters = {
   timeframe: 'Last 5 Years',
-  domain: 'All Domains',
+  subject_area: 'All Areas',
   subject_category: 'All Categories',
-  region: 'Global Distribution'
+  zone: 'Global Distribution'
 };
 
 const mapFiltersToParams = (filters) => {
   if (!filters) return {};
   const queryParams = {};
   
-  if (filters.subject_category && filters.subject_category !== 'All Categories') {
-    queryParams.subject_area = filters.subject_category;
+  if (filters.subject_area && filters.subject_area !== 'All Areas') {
+    queryParams.subject_area = filters.subject_area;
   } else if (filters.domain && filters.domain !== 'All Domains') {
     queryParams.subject_area = filters.domain;
   }
+
+  if (filters.subject_category && filters.subject_category !== 'All Categories') {
+    queryParams.subject_category = filters.subject_category;
+    if (!queryParams.subject_area) {
+      queryParams.subject_area = filters.subject_category;
+    }
+  }
+
+  if (filters.zone && filters.zone !== 'Global Distribution') {
+    queryParams.zone = filters.zone;
+  }
   
-  const currentYear = new Date().getFullYear() - 1;
+  const currentYear = new Date().getFullYear();
   if (filters.timeframe) {
     switch (filters.timeframe) {
       case 'Last Year':
